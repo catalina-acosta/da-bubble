4e33f3142f2fbf47d9c3785722c0662bc587f4b1
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-05-05 11:24:36 +0200

    project set up with angular, firebase and angular fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "dabubble-45298", appId: "1:313254431890:web:5259b5a1b479a3e35faa4b", storageBucket: "dabubble-45298.firebasestorage.app", apiKey: "AIzaSyBP6dPalFMzExvxYYWcUqS-wqOn7dK1Hcg", authDomain: "dabubble-45298.firebaseapp.com", messagingSenderId: "313254431890" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
