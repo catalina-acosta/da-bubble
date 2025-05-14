45d59b35830a6b1b23540b169cb2b9f5b720a765
Caryensong <caryensong@googlemail.com>
2025-05-14 16:47:12 +0200

    add workspace-menü btn style

---
+++ src/app/shared/component/menu-btn/menu-btn.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-btn',
+  imports: [],
+  templateUrl: './menu-btn.component.html',
+  styleUrl: './menu-btn.component.scss'
+})
+export class MenuBtnComponent {
+
+}
