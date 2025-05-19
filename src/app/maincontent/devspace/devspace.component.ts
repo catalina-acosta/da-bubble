e0cec0462c1e8acf0ee33b09f080cf333573901e
Caryensong <caryensong@googlemail.com>
2025-05-19 21:36:34 +0200

    implement user filtering and dynamic status styling

--- src/app/maincontent/devspace/devspace.component.ts
+++ src/app/maincontent/devspace/devspace.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FirebaseService } from '../../shared/services/firebase.service';
+import { UserInterface } from '../../shared/user.interface';
 
 @Component({
   selector: 'app-devspace',
-  imports: [],
+  imports: [CommonModule],
+  standalone:true,
   templateUrl: './devspace.component.html',
   styleUrl: './devspace.component.scss'
 })
 export class DevspaceComponent {
+firebase =inject(FirebaseService);
+allPersons: UserInterface[] = [];
+filteredPerson: UserInterface[] = []; 
+
+ngOnInit():void {
+  this.loadUsers();
+}
+
+async loadUsers(){
+ this.allPersons =await this.firebase.getUserList();
+ this.filteredPerson = this.allPersons;
+}
 
 }
